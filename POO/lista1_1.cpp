6b47a1d3efdf0514359933c05e8cce7cc76331b2
dudassis <164270706+dudassis@users.noreply.github.com>
2024-03-21 11:34:26 -0300

    Update lista1_1.cpp

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Agenda
+{
+    private:
+    string nome;
+    string telefone;
+    public:
+    
+void setNome(string nome)
+{
+    this->nome= nome;
+};
+
+string getNome()
+{
+    return nome;
+};
+
+void setTelefone(string telefone)
+{
+    this->telefone= telefone;
+};
+
+string getTelefone()
+{
+    return telefone;
+};
+
+void printAgenda()
+{
+    cout << nome <<"," << telefone << endl;
+}
+
+
+
+};
+int main()
+{
+    Agenda a;
+    a.setNome("maria");
+    a.setTelefone("6198453728");
+    
+    a.printAgenda();
+
+    return 0;
+}

3426c14d052881264f40e2433f1de9300493d4f8
dudassis <164270706+dudassis@users.noreply.github.com>
2024-03-21 22:52:10 -0300

    Update lista1_2.cpp

--- POO/lista1_2.cpp
+++ POO/lista1_2.cpp
@@ -1,1 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cadastro
+{
+    private:
+    string nome;
+    string matricula;
+    string curso;
+    
+    public:
+    
+void setNome(string nome)
+{
+    this->nome= nome;
+};
+
+string getNome()
+{
+    return nome;
+};
+
+void setMatricula(string matricula)
+{
+    this->matricula= matricula;
+};
+
+string getMatricula()
+{
+    return matricula;
+};
+
+void setCurso(string Curso)
+{
+    this->curso= curso;
+};
+
+string getCurso()
+{
+    return curso;
+};
+
+
+void printCadastro()
+{
+    cout << nome <<","<< matricula<<","<< curso<< endl;
+}
+
+
+
+};
+int main()
+{
+    Cadastro a;
+    a.setNome("Anna");
+    a.setMatricula("232567812");
+    a.setCurso("Engenharia");
+    
+    a.printCadastro();
+
+    return 0;
+}
 

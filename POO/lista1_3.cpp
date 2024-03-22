bce6ccd925f47f2d7e4a1f81a73620a423af94df
dudassis <164270706+dudassis@users.noreply.github.com>
2024-03-21 23:00:02 -0300

    Update lista1_3.cpp

--- POO/lista1_3.cpp
+++ POO/lista1_3.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Aluno
+{
+    private:
+    string nota;
+    string falta;
+    
+    
+    public:
+    
+void setNota(string nota)
+{
+    this->nota= nota;
+};
+
+string getNota()
+{
+    return nota;
+};
+
+void setFalta(string falta)
+{
+    this->falta= falta;
+};
+
+string getFalta()
+{
+    return falta;
+};
+
+
+
+void printAluno()
+{
+    cout << nota <<","<< falta<< endl;
+}
+
+
+
+};
+int main()
+{
+    Aluno a;
+    a.setNota(" Sua nota é 8");
+    a.setFalta("Voce tem 1 falta");
+    
+    a.printAluno();
+
+    return 0;
+}
 
